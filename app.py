11ff19dd1d40b19d2000d500ccf271e39a34279e
Iam_BlackShifu <anesuchiodza@gmail.com>
2019-05-11 11:55:17 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,68 @@
+import os
+import script
+import pprint as pp
+from flask import Flask, flash, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import recommendations
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return file.filename
+            
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('app.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    name=upload_file()
+    value=script.analysis("./uploads/"+name)
+    list_re = value.split()
+    #print(type(list_re))
+    test=(list_re[3])
+    
+    if test=="Bacterial":
+        reco=recommendations.bacterial_blight()
+
+    elif test=="Lateblight.":
+        reco=recommendations.lateblight()
+
+    elif test=="Healthy":
+        reco="This leaf is healthy"
+    else:
+        reco=recommendations.viral()
+
+
+    
+
+    return render_template('app.html',value=value, reco=reco,)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ recommendations.py
@@ -0,0 +1,30 @@
+def lateblight():
+    reco = "This disease overwinters in the soil around infected plants as well as on garden debris and seeds. It will also remain in the twig cankers, leaves, stems and fruit of infected trees."
+
+    reco += "\n\n1.Spraying with a baking soda solution (a tablespoon of baking soda, 2 1/2 tablespoons of vegetable oil, a teaspoon of liquid soap, not detergent, to one gallon of water), or neem oil (do not use when pollinating insects including bees or other beneficial insects are present). Baking soda may burn some plant leaves. Spray only a few and then check for a reaction before applying applications every two weeks.\n2.Apply sulfur sprays or copper-based fungicides weekly at first sign of disease to prevent its spread. These organic fungicides will not kill leaf spot, but prevent the spores from germinating.\n•	Safely treat most fungal and bacterial diseases with SERENADE Garden. This broad spectrum bio-fungicide uses a patented strain of Bacillus subtilis that is registered for organic use. Best of all, SERENADE is completely non-toxic to honey bees and beneficial insects.\n•	Containing copper and pyrethrins, Bonide® Garden Dust is a safe, one-step control for many insect attacks and fungal problems. For best results, cover both the tops and undersides of leaves with a thin uniform film or dust. Depending on foliage density, 10 oz will cover 625 sq ft. Repeat applications every 7-10 days, as needed.\n"
+    
+
+    return reco
+
+
+def viral():
+    reco = "Control this disease by planting resistant cultivars and growing your own transplants from disease-free seed. Apply Trichoderma harzianum to the soil just before planting. Promote good air circulation. For early blight, apply potassium bicarbonate (baking soda) sprays starting 2 weeks before the time of year when symptoms would normally first appear. Dispose of infected plants and when possible, use a 3-year rotation."
+    
+    return reco
+
+
+def bacterial_blight():
+    reco ="1.	When selecting fruit trees, choose resistant varieties if possible."
+
+    reco +=" \n2.   Keep the soil under the tree clean and rake up fallen fruit."
+    reco += " \n3.	Use a thick layer of mulch to cover the soil after you have raked and cleaned it well. Mulch will reduce weeds and prevent the disease pathogen from splashing back up onto the leaves."
+    reco +=" \n4.	Prune or stake plants to improve air circulation. Make sure to disinfect your pruning equipment(one part bleach to 4 parts water) after each cut."
+    reco +=" \n5.	Leaf spot among vegetables is most often introduced through infected seed or transplants. Make sure your seeds and transplants are from leaf spot-free stock."
+    reco +=" \nThere is no cure for plants infected with bacterial leaf spot. Preventive, organic measures include: "
+    reco += "  \n\t•	Spraying with a baking soda solution(a tablespoon of baking soda, 2 1/2 tablespoons of vegetable oil, a teaspoon of liquid soap, not detergent, to one gallon of water), or neem oil(do not use when pollinating insects including bees or other beneficial insects are present). Baking soda may burn some plant leaves. Spray only a few and then check for a reaction before applying applications every two weeks."
+    reco +="  \n\t•	Apply sulfur sprays or copper-based fungicides weekly at first sign of disease to prevent its spread. These organic fungicides will not kill leaf spot, but prevent the spores from germinating."
+    reco +="  \n\t•	Safely treat most fungal and bacterial diseases with SERENADE Garden. This broad spectrum bio-fungicide uses a patented strain of Bacillus subtilis that is registered for organic use. Best of all, SERENADE is completely non-toxic to honey bees and beneficial insects."
+    reco += "  \t•	Containing copper and pyrethrins, Bonide® Garden Dust is a safe, one-step control for many insect attacks and fungal problems. For best results, cover both the tops and undersides of leaves with a thin uniform film or dust. Depending on foliage density, 10 oz will cover 625 sq ft. Repeat applications every 7-10 days, as needed."
+
+
+    return reco
